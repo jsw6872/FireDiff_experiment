89568d294f29278617e3361e7b14f2eee95fd24e
seong woo <sw.cho@alcherainc.com>
2023-06-07 01:04:41 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FireDiff_experiment
+## Backend AI 초기 환경 세팅
+- `pip install torchvision --extra-index-url https://download.pytorch.org/whl/cu113`
+- `pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'`
+- 
--- main.py
+++ main.py
@@ -17,7 +17,8 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import copy
 
-import transforms as T
+from torchvision import transforms as T
+# import transforms as T
 
 
 def get_transform(train):
@@ -31,30 +32,33 @@ def get_transform(train):
 
 
 if __name__ == '__main__':
-    dataset_path = '/home/seongwoo/workspace/DataScience_ML-DL/DL/assignment/trash_detection/data' # Dataset 경로 지정 필요
-    train_path = dataset_path + '/train.json'
-    val_path = dataset_path + '/val.json'
+    dataset_path = '/home/work/jsw_workspace/detection/fire_data/' # Dataset 경로 지정 필요
+    train_path = dataset_path + 'train/train.json'
+    val_path = dataset_path + 'test/test.json'
+
+    train_img_path = dataset_path + 'train/fire'
+    val_img_path = dataset_path + 'test/fire'
     
     coco = COCO(train_path)
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     # use our dataset and defined transformations
-    dataset = COCO_dataformat(train_path, get_transform(train=True))
-    dataset_test = COCO_dataformat(val_path, get_transform(train=False))
+    dataset = COCO_dataformat(train_img_path, train_path)
+    dataset_test = COCO_dataformat(val_img_path, val_path)
 
     indices = torch.randperm(len(dataset)).tolist()
 
-    dataset = torch.utils.data.Subset(dataset, indices[:-400])
-    dataset_test = torch.utils.data.Subset(dataset_test, indices[-400:])
+    # dataset = torch.utils.data.Subset(dataset, indices[:-400])
+    # dataset_test = torch.utils.data.Subset(dataset_test, indices[-400:])
 
     
 
     data_loader = torch.utils.data.DataLoader(
-                                            dataset, batch_size=4, shuffle=True, num_workers=4,
+                                            dataset, batch_size=16, shuffle=True, num_workers=4,
                                             collate_fn=utils.collate_fn)
     data_loader_test = torch.utils.data.DataLoader(
-                                                dataset_test, batch_size=1, shuffle=False, num_workers=4,
+                                                dataset_test, batch_size=4, shuffle=False, num_workers=4,
                                                 collate_fn=utils.collate_fn)
 
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights = None, weights_backbone = ResNet50_Weights.IMAGENET1K_V2)#, weights_backbone = None)
@@ -72,22 +76,23 @@ if __name__ == '__main__':
     model.to(device)
 
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-    num_epochs = 10
+    optimizer = torch.optim.SGD(params, lr=0.05, momentum=0.9, weight_decay=0.0001)
+    num_epochs = 20
     loss_list = []
     for epoch in range(num_epochs):
     # train for one epoch, printing every 10 iterations
-        metric_logger, all_losses_dict_df = engine.train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=80)
+        metric_logger, all_losses_dict_df = engine.train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=100)
         epoch_loss = all_losses_dict_df.apply(lambda x: sum(x), axis=1).mean()
         loss_list.append(epoch_loss)
         
         # if epoch_loss <= min(loss_list):
         #     best_model_wts = copy.deepcopy(model.state_dict())
         # evaluate on the test dataset
-        engine.evaluate(model, data_loader_test, device=device)
+        print(f'---------{epoch+1}epoch evaluation---------')
+        evaluation = engine.evaluate(model, data_loader_test, device=device)
 
         best_model_wts = copy.deepcopy(model.state_dict())
-        model.load_state_dict(best_model_wts)  
+        model.load_state_dict(best_model_wts)
         torch.save(model,f'./model/pretrained_{epoch+1}.pt')
     
     print("Done!")
